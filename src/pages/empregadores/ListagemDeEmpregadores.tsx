2383d5030c6db8c783cc0523ad507266992ef2e9
leoavfag <leoavfag@gmail.com>
2023-03-23 16:20:47 -0300

    feat: add table for content, add loading component and pagination

--- src/pages/empregadores/ListagemDeEmpregadores.tsx
+++ src/pages/empregadores/ListagemDeEmpregadores.tsx
@@ -1,18 +1,59 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import {
+  LinearProgress,
+  Pagination,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+
+import { Environment } from '../../shared/environment';
 import { ListTool } from '../../shared/components';
+import { useDebounce } from '../../shared/hooks';
 import { BaseLayout } from '../../shared/layouts';
+import {
+  EmpregadoresService,
+  IEmpregador,
+} from '../../shared/services/api/empregadores/EmpregadoresService';
 
 export const ListagemDeEmpregadores: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(1000);
+
+  const [rows, setRows] = useState<IEmpregador[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const search = useMemo(() => {
-    if (searchParams.get('search') === '') {
-      searchParams.delete('search');
-      setSearchParams(searchParams);
-    }
     return searchParams.get('search') || '';
   }, [searchParams]);
+
+  const page = useMemo(() => {
+    return Number(searchParams.get('page') || '1');
+  }, [searchParams]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    debounce(() => {
+      EmpregadoresService.getAll(page, search).then((result) => {
+        setIsLoading(false);
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          console.log(result);
+
+          setRows(result.data);
+          setTotalCount(result.totalCount);
+        }
+      });
+    });
+  }, [search, page]);
 
   return (
     <BaseLayout
@@ -23,10 +64,71 @@ export const ListagemDeEmpregadores: React.FC = () => {
           newButtonText='Novo'
           searchText={search}
           onSearchTextChange={(text) =>
-            setSearchParams({ search: text }, { replace: true })
+            setSearchParams({ search: text, page: '1' }, { replace: true })
           }
         />
       }
-    ></BaseLayout>
+    >
+      <TableContainer
+        component={Paper}
+        variant='outlined'
+        sx={{ m: 1, width: 'auto' }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome</TableCell>
+              <TableCell>CNPJ</TableCell>
+              <TableCell>Tipo</TableCell>
+              <TableCell>Arranjo de pagamento</TableCell>
+              <TableCell>Total na Carteira</TableCell>
+              <TableCell>Ativo</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>Ações</TableCell>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.cnpj}</TableCell>
+                <TableCell>{row.employerType}</TableCell>
+                <TableCell>{row.paymentArrangementMnemonic}</TableCell>
+                <TableCell>{row.totalBalance}</TableCell>
+                <TableCell>{row.active ? 'Sim' : 'Nâo'}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          {totalCount === 0 && !isLoading && (
+            <caption>{Environment.EMPTY_LIST}</caption>
+          )}
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={7}>
+                  <LinearProgress variant='indeterminate' />
+                </TableCell>
+              </TableRow>
+            )}
+            {totalCount > 0 && totalCount > Environment.LINE_LIMIT && (
+              <TableRow>
+                <TableCell colSpan={7}>
+                  <Pagination
+                    page={page}
+                    count={Math.ceil(totalCount / Environment.LINE_LIMIT)}
+                    onChange={(e, newPage) =>
+                      setSearchParams(
+                        { search, page: newPage.toString() },
+                        { replace: false }
+                      )
+                    }
+                  />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </BaseLayout>
   );
 };
